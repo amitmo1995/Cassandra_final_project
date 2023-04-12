bcd97181f2f22a8b204f813f1bcfe478a1543f0b
amitmo1995 <99633944+amitmo1995@users.noreply.github.com>
2023-04-12 11:38:17 +0300

    Delete ConflictsCounter directory

--- ConflictsCounter/conflictsCounterByIndexes.py
+++
@@ -1,9 +0,0 @@
-def conflictCounterByIndexes(arr):
-    counter=0
-    for index,i in enumerate(arr):
-        if not(i==index+1) :
-            counter+=1
-    return counter
-
-arr = [1, 2, 4, 3]
-print("Number of conflicts:", conflictCounterByIndexes(arr))
